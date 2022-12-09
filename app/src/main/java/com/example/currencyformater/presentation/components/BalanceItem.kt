c704656dc4034210e1bec2dac9b3d0e360a5f1cc
romanos <romanos.panagakos@gmail.com>
2022-12-09 12:14:46 +0200

    Add: compose items

---
+++ app/src/main/java/com/example/currencyformater/presentation/components/BalanceItem.kt
@@ -0,0 +1,61 @@
+package com.example.currencyformater.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.currencyformater.common.annotations.DevicePreview
+import com.example.currencyformater.theme.LocalTheme
+
+@Composable
+fun BalanceItem(
+    modifier: Modifier = Modifier,
+    priceBalance: String,
+    currencyBalance: String
+) {
+
+    Column(
+        modifier = modifier
+            .background(
+            color = LocalTheme.colors.White,
+            shape = RoundedCornerShape(LocalTheme.radius.radius_8dp)
+        )
+            .defaultMinSize(minWidth = 80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(
+                    top = LocalTheme.spacing.padding_8dp,
+                    start = LocalTheme.spacing.padding_4dp,
+                    end = LocalTheme.spacing.padding_4dp
+                ),
+            text = priceBalance,
+            style = LocalTheme.typography.REGULAR_16_MONT,
+            color = LocalTheme.colors.PrussianBlue
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            modifier = Modifier
+                .padding(
+                    start = LocalTheme.spacing.padding_4dp,
+                    end = LocalTheme.spacing.padding_4dp,
+                    bottom = LocalTheme.spacing.padding_8dp
+                ),
+            text = currencyBalance,
+            style = LocalTheme.typography.BOLD_14_MONT,
+            color = LocalTheme.colors.SteelTeal
+        )
+    }
+}
+
+@Composable
+@DevicePreview
+fun BalanceItemPreview(){
+    BalanceItem(priceBalance = "1200$", currencyBalance = "USD")
+}
---
+++ app/src/main/java/com/example/currencyformater/presentation/components/HeaderTitleItem.kt
@@ -0,0 +1,39 @@
+package com.example.currencyformater.presentation.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import com.example.currencyformater.common.annotations.DevicePreview
+import com.example.currencyformater.theme.LocalTheme
+
+@Composable
+fun HeaderTitleItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    textColor: Color,
+    textStyle: TextStyle
+) {
+    Surface(modifier = modifier) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = title,
+            style = textStyle,
+            color = textColor
+        )
+    }
+}
+
+@Composable
+@DevicePreview
+private fun HeaderTitleItemPreview() {
+    HeaderTitleItem(
+        title = "Currency exchange",
+        textColor = LocalTheme.colors.MountainMeadow,
+        textStyle = LocalTheme.typography.REGULAR_12_MONT
+    )
+}
