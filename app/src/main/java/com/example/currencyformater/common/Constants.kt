789ca2c879b33200f23e03a16a6d07274ba01877
romanos <romanos.panagakos@gmail.com>
2022-12-11 17:42:17 +0200

    Ready

--- app/src/main/java/com/example/currencyformater/common/Constants.kt
+++ app/src/main/java/com/example/currencyformater/common/Constants.kt
@@ -3,7 +3,7 @@ package com.example.currencyformater.common
 object Constants {
 
     const val BASE_URL = "https://api.apilayer.com/"
-    const val API_KEY = "i1PMfVUbzMxWJlyZ8ONvqnjbdMZnbKYF"
+    const val API_KEY = ""
     const val SHARED_PREF = "currency_formatter"
     const val APP_DATABASE = "app_database"
 }
